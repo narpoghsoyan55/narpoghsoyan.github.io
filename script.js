895588549f009d9c6b45e551fad80a2a57d03325
narpoghsoyan55 <109800459+narpoghsoyan55@users.noreply.github.com>
2022-08-23 00:34:39 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,210 @@
+const click = document.getElementsByClassName("ascript")[0];
+click.onclick = function(){
+alert("Թեստը չեմ կազմել, քանի որ դրա համար անհրաժեշտ էր գրանցվել կայքում՝ գրել տվյալները և տվյալների ճիշտ կամ սխալ լինելը պետք է ստուգվեր այլ ծրագրավորման լեզվի առկայությամբ այդ իսկ պատճառով չգրեցի");
+}
+
+const ddd = document.getElementsByClassName("btn")[0];
+  ddd.onclick = function(){
+      alert("Շնորհակալություն\n Շուտով մենք կապ կհաստատենք և կտրամադրենք ավել\n տեղեկատվություն, այն մասին, թէ ինչպես կարող եք լեզու\n սովորել և բացել նոր աշխարհ Բեռլից ի հետ։ Մինչ այդ,\n իմացեք ավելին մեր մասին");
+  }
+
+  var a = document.getElementsByClassName("menu2")[0];
+ var b = document.getElementsByClassName("car")[0];
+ 
+ a.onmouseenter = function(){
+ b.style.display = "block";
+ }
+ 
+ a.onmouseleave = function(){
+ b.style.display = "none";
+ }
+ 
+ b.onmouseleave = function(){
+ b.style.display = "none";
+ }
+ 
+ b.onmouseenter = function(){
+ b.style.display = "block";
+ }
+
+ var c = document.getElementsByClassName("menu3")[0];
+ var d = document.getElementsByClassName("cav")[0];
+ 
+ c.onmouseenter = function(){
+ d.style.display = "block";
+ }
+ 
+ c.onmouseleave = function(){
+ d.style.display = "none";
+ }
+ 
+ d.onmouseleave = function(){
+ d.style.display = "none";
+ }
+ 
+ d.onmouseenter = function(){
+ d.style.display = "block";
+ }
+
+ var e = document.getElementsByClassName("menu4")[0];
+ var f = document.getElementsByClassName("cap")[0];
+ 
+ e.onmouseenter = function(){
+ f.style.display = "block";
+ }
+ 
+ e.onmouseleave = function(){
+ f.style.display = "none";
+ }
+ 
+ f.onmouseleave = function(){
+ f.style.display = "none";
+ }
+ 
+ f.onmouseenter = function(){
+ f.style.display = "block";
+ }
+
+ var g = document.getElementsByClassName("menu5")[0];
+ var h = document.getElementsByClassName("can")[0];
+ 
+ g.onmouseenter = function(){
+ h.style.display = "block";
+ }
+ 
+ g.onmouseleave = function(){
+ h.style.display = "none";
+ }
+ 
+ h.onmouseleave = function(){
+ h.style.display = "none";
+ }
+ 
+ h.onmouseenter = function(){
+ h.style.display = "block";
+ }
+
+ var p = document.getElementsByClassName("menu6")[0];
+ var k = document.getElementsByClassName("caf")[0];
+ 
+ p.onmouseenter = function(){
+ k.style.display = "block";
+ }
+ 
+ p.onmouseleave = function(){
+ k.style.display = "none";
+ }
+ 
+ k.onmouseleave = function(){
+ k.style.display = "none";
+ }
+ 
+ k.onmouseenter = function(){
+ k.style.display = "block";
+ }
+
+ var lastScrollTop = 500;
+ navbar = document.getElementById("navbar");
+ window.addEventListener("scroll", function(){
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if(scrollTop > lastScrollTop){
+    navbar.style.display = "block";
+  }else{
+    navbar.style.display = "none";
+  }
+ })
+
+ var s1 = document.getElementsByClassName("menus")[0];
+ var s2 = document.getElementsByClassName("cars")[0];
+ 
+ s1.onmouseenter = function(){
+ s2.style.display = "block";
+ }
+ 
+ s1.onmouseleave = function(){
+ s2.style.display = "none";
+ }
+ 
+ s2.onmouseleave = function(){
+ s2.style.display = "none";
+ }
+ 
+ s2.onmouseenter = function(){
+ s2.style.display = "block";
+ }
+
+ var s3 = document.getElementsByClassName("menus2")[0];
+ var s4 = document.getElementsByClassName("cavs")[0];
+ 
+ s3.onmouseenter = function(){
+ s4.style.display = "block";
+ }
+ 
+ s3.onmouseleave = function(){
+ s4.style.display = "none";
+ }
+ 
+ s4.onmouseleave = function(){
+ s4.style.display = "none";
+ }
+ 
+ s4.onmouseenter = function(){
+ s4.style.display = "block";
+ }
+
+ var s5 = document.getElementsByClassName("menus3")[0];
+ var s6 = document.getElementsByClassName("caps")[0];
+ 
+ s5.onmouseenter = function(){
+ s6.style.display = "block";
+ }
+ 
+ s5.onmouseleave = function(){
+ s6.style.display = "none";
+ }
+ 
+ s6.onmouseleave = function(){
+ s6.style.display = "none";
+ }
+ 
+ s6.onmouseenter = function(){
+ s6.style.display = "block";
+ }
+
+ var s7 = document.getElementsByClassName("menus4")[0];
+ var s8 = document.getElementsByClassName("cans")[0];
+ 
+ s7.onmouseenter = function(){
+ s8.style.display = "block";
+ }
+ 
+ s7.onmouseleave = function(){
+ s8.style.display = "none";
+ }
+ 
+ s8.onmouseleave = function(){
+ s8.style.display = "none";
+ }
+ 
+ s8.onmouseenter = function(){
+ s8.style.display = "block";
+ }
+
+ var s9 = document.getElementsByClassName("menus5")[0];
+ var s10 = document.getElementsByClassName("cafs")[0];
+ 
+ s9.onmouseenter = function(){
+ s10.style.display = "block";
+ }
+ 
+ s9.onmouseleave = function(){
+ s10.style.display = "none";
+ }
+ 
+ s10.onmouseleave = function(){
+ s10.style.display = "none";
+ }
+ 
+ s10.onmouseenter = function(){
+ s10.style.display = "block";
+ }
